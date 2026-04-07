c4106901dc7fcae8ba31d73f41e81019ffe07d09
SANDEEP316244 <sandeep.adari007@gmail.com>
2026-04-07 20:29:49 +0530

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,10 +1,11 @@
-resource "aws_instance" "test" {
+resource "aws_instance" "ec2" {
     ami = "ami-0220d79f3f480ecf5"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.allow_all.id]
-    tags = {
-        Name = "test-1"
+    instance_type = "t3a.micro"
+    vpc_security_group_ids = [aws_security_group.sec-grp-test.id]
+    tags  {
+        Name = "test-DevOps-ec2"
         Application = "DevOps"
+        appgroup = "DevOps"
         DR = "NA"
         ManagedBy = "AwsConsole"
         Environment = "nonProd"
@@ -12,32 +13,27 @@ resource "aws_instance" "test" {
 }
 
 resource "aws_security_group" "allow_all" {
-  name   = "allow-all"
+  name = "allow-all"
 
   egress {
-    from_port        = 0 # from port 0 to to port 0 means all ports
-    to_port          = 0 
-<<<<<<< HEAD
-    protocol         = "443" # -1 means all protocols
-=======
-    protocol         = "tcp" # -1 means all protocols
->>>>>>> temp-fix
-    cidr_blocks      = ["0.0.0.0/0"] # internet
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port        = 0 # from port 0 to to port 0 means all ports
-    to_port          = 0 
-    protocol         = "443" # -1 means all protocols
-    cidr_blocks      = ["0.0.0.0/0"] # internet
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-    tags = {
-        Name = "test-1"
-        Application = "DevOps"
-        DR = "NA"
-        ManagedBy = "AwsConsole"
-        Environment = "nonProd"
-    }
-
+  tags = {
+    Name        = "test-1"
+    Application = "DevOps"
+    DR          = "NA"
+    ManagedBy   = "AwsConsole"
+    Environment = "nonProd"
+  }
 }
